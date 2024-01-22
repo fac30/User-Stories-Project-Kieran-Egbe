65feccb7085a3c55312f5d7fca7b8d2c032f9bb5
blackNoir2 <109542044+blackNoir2@users.noreply.github.com>
2024-01-22 21:07:41 +0000

    Finish the contact form

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # FAC-User-Stories-project
 Project 1 for FAC 30: to create a website that showcases user stories
 
-Visit the site here (To be completed)
-
  https://fac30.github.io/User-Stories-Project-Kieran-Egbe/
--- static/js/utils.js
+++ static/js/utils.js
@@ -39,4 +39,32 @@ function isElementValid(element, errorMsg) {
 
 
 
-export {isElementValid}
+/**
+ * Asynchronously loads the content of a file from the specified file path.
+ *
+ * @param {string} filePath - The path to the file to be loaded.
+ * @returns {Promise<string>} A Promise that resolves with the text content of the file.
+ * @throws {Error} Throws an error if there is an issue with fetching or reading the file.
+ *
+ * @example
+ * // how to use it example:
+ * try {
+ *   const fileContent = await loadFile("path/to/file.txt");
+ *   console.log(fileContent); // Display the content of the loaded file
+ * } catch (error) {
+ *   console.error('Error:', error);
+ * }
+ */
+async function loadFile(filePath) {
+    try {
+        const response = await fetch(filePath);
+        const data = await response.text();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+
+export {isElementValid, loadFile}
