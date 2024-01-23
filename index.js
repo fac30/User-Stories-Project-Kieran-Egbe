4e36eeec5873e931afe0f48ec00da33e3eca5d9e
giantflyingegg <abwaham@gmail.com>
2024-01-23 16:25:42 +0000

    responsive nav but elements need adjusting

--- index.js
+++ index.js
@@ -325,7 +325,14 @@ function openReadMoreSideBar() {
     }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var hamburger = document.querySelector('.hamburger-menu');
+    var navLinks = document.querySelector('.nav-links');
 
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
 
 
 
